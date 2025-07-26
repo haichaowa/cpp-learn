13aeab6de5b1cd571b4d1a283f03f501f26fb00d
haichaowang <1574427203@qq.com>
2025-07-26 19:33:46 +0800

    leetcode: 01.twosum

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cpp-learn
 c++个人学习记录
+
+## cmake
+https://blog.csdn.net/Yu_Lijing/article/details/149364720
+
+## clion
+### 远程开发
+https://blog.csdn.net/weixin_74027669/article/details/142391303
+
+/usr/bin/make 不是 /usr/bin/cmake
---
+++ leetcode/hot100/01_TwoSum.cpp
@@ -0,0 +1,30 @@
+//
+// Created by haichao on 2025-07-26.
+//
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> um;
+        for(int i=0; i<nums.size(); i++){
+            auto it = um.find(target-nums[i]);
+            if(it != um.end()){
+                return {um[target-nums[i]], i};
+            }
+            um[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+int main(){
+    Solution s;
+    std::vector<int> nums{2,7,11,15};
+    auto res = s.twoSum(nums, 9);
+    for(int i=0; i< res.size(); i++){
+        std::cout<<res[i]<<std::endl;
+    }
+}
